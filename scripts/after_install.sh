253ddb40b60ae6646df022c2c95b53355ca31d5e
Sreevas-MK <sreevasmk@gmail.com>
2026-03-08 05:20:19 +0000

    Modified after_install.sh for cloud-init to complete

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,6 +2,9 @@
 APP_DIR="/var/s3-nodejs-app"
 APP_USER="ec2-user"
 
+sudo cloud-init status --wait
+export PATH=$PATH:/usr/bin:/usr/local/bin
+
 echo "Installing dependencies..."
 cd $APP_DIR
 sudo -u $APP_USER npm install
